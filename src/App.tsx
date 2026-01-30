98ac0266821ffef9c293adb4ec833154139b0d21
SchlauesKind <roma.volt1125@gmail.com>
2026-01-30 20:51:13 +0100

    Added a side-submenu for navigation. (No actual navigation:)

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,12 @@ function App() {
       <HeaderComponent onMenuToggle={() => setMenuOpen((o) => !o)} />
 
       <div className="layout">
-        {menuOpen && (
-          <aside className="sidebar">
-            <SidebarComponent />
-          </aside>
-        )}
+        <SidebarComponent isOpen={menuOpen} />
         <main className="main">
           <h1>This is a new header</h1>
           <p>
             This content is nonsence and will be deleted soon{" "}
-            <span> in development</span>
+            <span> in development)</span>
           </p>
         </main>
       </div>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,28 @@
 import styles from "./Sidebar.module.css";
 
-function SidebarComponent() {
-  return <aside className={styles.sidebar}></aside>;
+function SidebarComponent({ isOpen }: { isOpen: boolean }) {
+  return (
+    <aside className={`${styles.sidebar} ${isOpen ? styles.expanded : ""}`}>
+      <h2>Sidebar Menu</h2>
+      <ul>
+        <li>
+          <a href="#">1</a>
+        </li>
+        <li>
+          <a href="#">2</a>
+        </li>
+        <li>
+          <a href="#">3</a>
+        </li>
+        <li>
+          <a href="#">4</a>
+        </li>
+        <li>
+          <a href="#">5</a>
+        </li>
+      </ul>
+    </aside>
+  );
 }
 
 export default SidebarComponent;
