482d5b7362d27af5dbd3f081e3d64bc2c0fe0b38
SchlauesKind <roma.volt1125@gmail.com>
2026-02-01 22:02:47 +0100

    Made a better layout for sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import cheese_icon from "../../assets/cheese_icon.png";
 import apple_icon from "../../assets/apple_icon.png";
+import home_icon from "../../assets/home_icon.svg";
 import flex_styles from "../../styles/FlexContainer.module.css";
 import styles from "./Sidebar.module.css";
 
@@ -9,7 +10,7 @@ function SidebarComponent() {
     <aside className={styles.sidebar}>
       <div className={`${flex_styles.flexContainer} ${styles.logoContainer}`}>
         <h1>AppleCheese</h1>
-        <img src={apple_icon} alt="Apple Icon" />
+        <img className={styles.sidebarIcon} src={apple_icon} alt="Apple Icon" />
         <p
           style={{
             fontSize: "1.5rem",
@@ -21,16 +22,25 @@ function SidebarComponent() {
           {" "}
           &{" "}
         </p>
-        <img src={cheese_icon} alt="Cheese Icon" />
+        <img
+          className={styles.sidebarIcon}
+          src={cheese_icon}
+          alt="Cheese Icon"
+        />
       </div>
       <ul>
         <li>
           <NavLink
             to="/"
             className={({ isActive }) =>
-              `${styles.navLink} ${isActive ? styles.active : ""}`
+              `${styles.navLink} ${isActive ? styles.active : ""} ${flex_styles.flexContainer}`
             }
           >
+            <img
+              className={styles.sidebarNavIcon}
+              src={home_icon}
+              alt="Home Icon"
+            />
             Homepage
           </NavLink>
         </li>
